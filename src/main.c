94f9c940e646e48186d6b351a562dc1993d97bba
Asher Vitek <asher.vitek@gmail.com>
2025-01-17 13:26:48 -0600

    Chapter 16 complete. woo!

--- src/main.c
+++ src/main.c
@@ -2,67 +2,64 @@
 #include "common.h"
 #include "debug.h"
 #include "vm.h"
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void testExpressions(Chunk *chunk) {
-    int constant = addConstant(chunk, 1.2);
-    writeChunk(chunk, OP_CONSTANT, 123);
-    writeChunk(chunk, constant, 123);
+static void repl() {
+    char line[1024];
 
-    constant = addConstant(chunk, 3.4);
-    writeChunk(chunk, OP_CONSTANT, 123);
-    writeChunk(chunk, constant, 123);
+    for (;;) {
+        printf("> ");
 
-    writeChunk(chunk, OP_ADD, 123);
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
 
-    constant = addConstant(chunk, 5.6);
-    writeChunk(chunk, OP_CONSTANT, 123);
-    writeChunk(chunk, constant, 123);
-
-    writeChunk(chunk, OP_DIVIDE, 123);
-    writeChunk(chunk, OP_NEGATE, 123);
-
-    writeChunk(chunk, OP_RETURN, 123);
+        interpret(line);
+    }
 }
 
-void writeConstant(Chunk *chunk, double value) {
-    int constant = addConstant(chunk, value);
-    writeChunk(chunk, OP_CONSTANT, 123);
-    writeChunk(chunk, constant, 123);
+static char *readFile(const char *path) {
+    FILE *file = fopen(path, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "Could not open file \"%s\".\n", path);
+        exit(74);
+    }
+
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);
+
+    char *buffer = (char *)malloc(fileSize + 1);
+    if (buffer == NULL) {
+        fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+        exit(74);
+    }
+
+    size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+    if (bytesRead < fileSize) {
+        fprintf(stderr, "Could not read file \"%s\".\n", path);
+        exit(74);
+    }
+    buffer[bytesRead] = '\0';
+
+    fclose(file);
+
+    return buffer;
 }
 
-void testExpressions2(Chunk *chunk) {
-    writeConstant(chunk, 2);
-    writeConstant(chunk, 3);
+static void runFile(const char *path) {
+    char *source = readFile(path);
+    InterpretResult result = interpret(source);
+    free(source);
 
-    writeChunk(chunk, OP_MULTIPLY, 123);
-
-    writeConstant(chunk, 1);
-
-    writeChunk(chunk, OP_ADD, 123);
-
-    writeConstant(chunk, 4);
-    writeConstant(chunk, 5);
-
-    writeChunk(chunk, OP_NEGATE, 123);
-    writeChunk(chunk, OP_DIVIDE, 123);
-    writeChunk(chunk, OP_SUBTRACT, 123);
-
-    writeChunk(chunk, OP_RETURN, 123);
-}
-
-void testExpressions3(Chunk *chunk) {
-    writeConstant(chunk, 4);
-    writeConstant(chunk, 3);
-
-    writeChunk(chunk, OP_SUBTRACT, 123);
-    writeChunk(chunk, OP_MULTIPLY, 123);
-
-    writeConstant(chunk, 0);
-    writeConstant(chunk, 2);
-    writeChunk(chunk, OP_SUBTRACT, 123);
-
-    writeChunk(chunk, OP_RETURN, 123);
+    if (result == INTERPRET_COMPILE_ERROR)
+        exit(65);
+    if (result == INTERPRET_RUNTIME_ERROR)
+        exit(60);
 }
 
 int main(int argc, const char *argv[]) {
@@ -70,21 +67,17 @@ int main(int argc, const char *argv[]) {
     for (int i = 1; i < 10000000; i++) {
 #endif
         initVM();
-        Chunk chunk;
-        initChunk(&chunk);
 
-        testExpressions2(&chunk);
+        if (argc == 1) {
+            repl();
+        } else if (argc == 2) {
+            runFile(argv[1]);
+        } else {
+            fprintf(stderr, "Usage: clox [path]\n");
+            exit(64);
+        }
 
-        /* writeConstant(&chunk, 1); */
-        /* writeConstant(&chunk, 1); */
-        /**/
-        /* writeChunk(&chunk, OP_ADD, 123); */
-        /* writeChunk(&chunk, OP_RETURN, 123); */
-
-        /* disassembleChunk(&chunk, "test chunk"); */
-        interpret(&chunk);
         freeVM();
-        freeChunk(&chunk);
 #ifdef PERFORMANCE_TEST
     }
 #endif
