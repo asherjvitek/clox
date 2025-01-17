6c46c0e54b32c90d96641dcd4ce6902b160f61b1
Asher Vitek <asher.vitek@gmail.com>
2025-01-17 07:53:31 -0600

    chapter 15 done! I think that I got all the challenges done and there is a small performance difference between the popping and then pushing back on rather than using the peek method that I created to modify the value that is on the top of the stack.

--- src/main.c
+++ src/main.c
@@ -1,20 +1,93 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "vm.h"
 #include <stdio.h>
 
-int main(int argc, const char* argv[]) {
-    Chunk chunk;
-    initChunk(&chunk);
+void testExpressions(Chunk *chunk) {
+    int constant = addConstant(chunk, 1.2);
+    writeChunk(chunk, OP_CONSTANT, 123);
+    writeChunk(chunk, constant, 123);
 
-    int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
+    constant = addConstant(chunk, 3.4);
+    writeChunk(chunk, OP_CONSTANT, 123);
+    writeChunk(chunk, constant, 123);
 
-    writeChunk(&chunk, OP_RETURN, 123);
+    writeChunk(chunk, OP_ADD, 123);
 
-    disassembleChunk(&chunk, "test chunk");
-    freeChunk(&chunk);
+    constant = addConstant(chunk, 5.6);
+    writeChunk(chunk, OP_CONSTANT, 123);
+    writeChunk(chunk, constant, 123);
+
+    writeChunk(chunk, OP_DIVIDE, 123);
+    writeChunk(chunk, OP_NEGATE, 123);
+
+    writeChunk(chunk, OP_RETURN, 123);
+}
+
+void writeConstant(Chunk *chunk, double value) {
+    int constant = addConstant(chunk, value);
+    writeChunk(chunk, OP_CONSTANT, 123);
+    writeChunk(chunk, constant, 123);
+}
+
+void testExpressions2(Chunk *chunk) {
+    writeConstant(chunk, 2);
+    writeConstant(chunk, 3);
+
+    writeChunk(chunk, OP_MULTIPLY, 123);
+
+    writeConstant(chunk, 1);
+
+    writeChunk(chunk, OP_ADD, 123);
+
+    writeConstant(chunk, 4);
+    writeConstant(chunk, 5);
+
+    writeChunk(chunk, OP_NEGATE, 123);
+    writeChunk(chunk, OP_DIVIDE, 123);
+    writeChunk(chunk, OP_SUBTRACT, 123);
+
+    writeChunk(chunk, OP_RETURN, 123);
+}
+
+void testExpressions3(Chunk *chunk) {
+    writeConstant(chunk, 4);
+    writeConstant(chunk, 3);
+
+    writeChunk(chunk, OP_SUBTRACT, 123);
+    writeChunk(chunk, OP_MULTIPLY, 123);
+
+    writeConstant(chunk, 0);
+    writeConstant(chunk, 2);
+    writeChunk(chunk, OP_SUBTRACT, 123);
+
+    writeChunk(chunk, OP_RETURN, 123);
+}
+
+int main(int argc, const char *argv[]) {
+#ifdef PERFORMANCE_TEST
+    for (int i = 1; i < 10000000; i++) {
+#endif
+        initVM();
+        Chunk chunk;
+        initChunk(&chunk);
+
+        testExpressions2(&chunk);
+
+        /* writeConstant(&chunk, 1); */
+        /* writeConstant(&chunk, 1); */
+        /**/
+        /* writeChunk(&chunk, OP_ADD, 123); */
+        /* writeChunk(&chunk, OP_RETURN, 123); */
+
+        /* disassembleChunk(&chunk, "test chunk"); */
+        interpret(&chunk);
+        freeVM();
+        freeChunk(&chunk);
+#ifdef PERFORMANCE_TEST
+    }
+#endif
 
     return 0;
 }
