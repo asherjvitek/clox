e2cfc9cd1779c2a113f093a2968abf3730fd71bd
Asher Vitek <asher.vitek@gmail.com>
2025-01-20 07:54:50 -0600

    we are partway through 18

--- src/value.c
+++ src/value.c
@@ -15,7 +15,8 @@ void writeValueArray(ValueArray *array, Value value) {
         int oldCapacity = array->capacity;
 
         array->capacity = GROW_CAPACITY(oldCapacity);
-        array->values = GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
+        array->values =
+            GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
     }
 
     array->values[array->count] = value;
@@ -28,5 +29,26 @@ void freeValueArray(ValueArray *array) {
 }
 
 void printValue(Value value) {
-    printf("%g", value);
+    switch (value.type) {
+        case VAL_BOOL:
+            printf(AS_BOOL(value) ? "true" : "false");
+            break;
+        case VAL_NIL:
+            printf("nil");
+            break;
+        case VAL_NUMBER:
+            printf("%g", AS_NUMBER(value));
+            break;
+    }
+}
+
+bool valuesEqual(Value a, Value b) {
+    if (a.type != b.type) return false;
+
+    switch (a.type) {
+        case VAL_BOOL: return AS_BOOL(a) == AS_BOOL(b);
+        case VAL_NIL: return true;
+        case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+        default: return false;
+    }
 }
