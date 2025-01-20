5f27bf31565a8ca045b202a777fe31a6f73e3144
Asher Vitek <asher.vitek@gmail.com>
2025-01-20 14:21:13 -0600

    finished with chapter 18!

--- src/vm.c
+++ src/vm.c
@@ -85,24 +85,15 @@ static InterpretResult run() {
                 push(BOOL_VAL(valuesEqual(a, b)));
                 break;
             }
-            case OP_ADD:
-                BINARY_OP(NUMBER_VAL, +);
-                break;
-            case OP_SUBTRACT:
-                BINARY_OP(NUMBER_VAL, -);
-                break;
-            case OP_MULTIPLY:
-                BINARY_OP(NUMBER_VAL, *);
-                break;
-            case OP_DIVIDE:
-                BINARY_OP(NUMBER_VAL, /);
-                break;
+            case OP_ADD: BINARY_OP(NUMBER_VAL, +); break;
+            case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
+            case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
+            case OP_DIVIDE: BINARY_OP(NUMBER_VAL, /); break;
             case OP_NOT:
                 push(BOOL_VAL(isFalsey(pop())));
                 break;
-            // This is the normal implementation that the book would do
-            // I changed this to use the optimized version.
-            //  case OP_NEGATE: push(-pop()); break;
+            case OP_GREATER: BINARY_OP(BOOL_VAL, >); break;
+            case OP_LESS: BINARY_OP(BOOL_VAL, <); break;
             case OP_NEGATE:
                 if (!IS_NUMBER(peek(0))) {
                     runtimeError("Operand must be a number.");
